4fa7022034f5bb21443cdfafbb145e2d9812f04a
Ariane Lima Carvalho <carvalho.ariane@escola.pr.gov.br>
2026-03-20 07:31:43 -0300

    Create database schema for book store

---
+++ sistema-livraria.sql
@@ -0,0 +1,20 @@
+-- Cria o banco de dados
+--CREATE DATABASE IF NOT EXISTS Livraria;
+
+-- Seleciona o banco de dados
+--USE Livraria;
+
+-- Cria a tabela Clientes
+CREATE TABLE IF NOT EXISTS Clientes (
+    ID INT AUTO_INCREMENT PRIMARY KEY,
+    nomeCliente VARCHAR(100) NOT NULL,
+    emailCliente VARCHAR(100) UNIQUE NOT NULL
+);
+
+-- Cria a tabela Compras
+CREATE TABLE IF NOT EXISTS Compras (
+    CompraID INT AUTO_INCREMENT PRIMARY KEY,
+    ClienteID INT NOT NULL,
+    NomeLivro VARCHAR(150) NOT NULL,
+    FOREIGN KEY (ClienteID) REFERENCES Clientes(ID)
+);
