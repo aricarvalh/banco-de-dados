20cc5b979b56e19a61795af7ebdf6258cf84ba66
Ariane Lima Carvalho <carvalho.ariane@escola.pr.gov.br>
2026-04-09 10:42:35 -0300

    Update testes-pragma.sql

--- testes-pragma.sql
+++ testes-pragma.sql
@@ -26,3 +26,8 @@ INSERT INTO Clientes (Nome) VALUES ('Bruno');
 
 -- Teste da FK ou chave estrangeira
 INSERT INTO Pedidos (ClienteID, DataPedido) VALUES (99, '2026-04-08');
+
+
+
+-- Verificação da FK (teste 2) - executar separado
+-- PRAGMA foreign_key_list(Pedidos);
